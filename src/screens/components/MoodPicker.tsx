7160c0ab8c8728fc89eed6b789446a0b39597263
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2021-11-29 16:53:45 +0100

    Setup the Bottom navigation and home screen

---
+++ src/screens/components/MoodPicker.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const moodOptions = [
+  { emoji: '🧑‍💻', description: 'studious' },
+  { emoji: '🤔', description: 'pensive' },
+  { emoji: '😊', description: 'happy' },
+  { emoji: '🥳', description: 'celebratory' },
+  { emoji: '😤', description: 'frustrated' },
+];
+
+export const MoodPicker: React.FC = () => {
+  return (
+    <View style={styles.moodOptions}>
+      {moodOptions.map(option => (
+        <Text key={option.emoji}>{option.emoji}</Text>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  moodOptions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+});
