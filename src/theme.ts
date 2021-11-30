201817fbd0fcdff1cbebb6e1fe06121531fd39cf
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2021-11-30 12:22:29 +0100

    Add custom font to the app

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,14 @@
 export const theme = {
+  //colors
+
   colorPurple: '#454C73',
   colorWhite: '#FFF',
   colorLavender: '#87677B',
   colorBlue: '#1D84B5',
   colorGrey: '#8E9AAF',
+
+  //fonts
+  fontFamilyBold: 'Kalam-Bold',
+  fontFamilyRegular: 'Kalam-Regular',
+  fontFamilyLight: 'Kalam-Light',
 };
