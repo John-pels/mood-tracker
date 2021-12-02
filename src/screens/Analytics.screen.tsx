0b51b2dd14157e020e4b8f9a878abb9e4a2fa44a
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2021-12-02 02:22:14 +0100

    Add pie chart

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,28 @@
+import { groupBy } from 'lodash';
 import React from 'react';
-import { Text, View } from 'react-native';
-
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
+import { StyleSheet, View } from 'react-native';
 export const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
-    <View>
-      <Text>Analytics</Text>
+    <View style={styles.container}>
+      <VictoryPie data={data} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
